e49b010a53a9cb60b0e7aeb3a9db0cd720ea74e5
Subrata Dev Mohanta <108514630+Subratadevmohanta@users.noreply.github.com>
2023-06-14 12:13:30 +0600

    12-26member

--- src/components/Member.js
+++ src/components/Member.js
@@ -11,7 +11,21 @@ import IMG8 from "../assets/Img8.jpg"
 import IMG9 from "../assets/Img9.jpg"
 import IMG10 from "../assets/Img10.jpg"
 import IMG11 from "../assets/Img11.jpg"
-
+import IMG12 from "../assets/Img12.jpg"
+import IMG13 from "../assets/Img13.jpg"
+import IMG14 from "../assets/Img14.jpg"
+import IMG15 from "../assets/Img15.jpg"
+import IMG16 from "../assets/Img16.jpg"
+import IMG17 from "../assets/Img17.jpg"
+import IMG18 from "../assets/Img18.jpg"
+import IMG19 from "../assets/Img19.jpg"
+import IMG20 from "../assets/Img20.jpg"
+import IMG21 from "../assets/Img21.jpg"
+import IMG22 from "../assets/Img22.jpg"
+import IMG23 from "../assets/Img23.jpg"
+import IMG24 from "../assets/Img24.jpg"
+import IMG25 from "../assets/Img25.jpg"
+import IMG26 from "../assets/Img26.jpg"
 
 
 import React from 'react'
@@ -146,6 +160,186 @@ const Member = () => {
             </div>
             <div className="cdetail">
                 <h2>Puja Mohanta.</h2>
+                <br/>
+                <br/>
+                <Link to="" className="btn">WebSite</Link>
+                <Link to="" className="btn btn-light">FaceBook</Link>
+            </div>
+        </div>
+        <div className="mycard">
+            <div className="cimg">
+                <img src={IMG12} alt="IMG6"/>
+            </div>
+            <div className="cdetail">
+                <h2>Sumon Mohanta.</h2>
+                <br/>
+                <br/>
+                <Link to="" className="btn">WebSite</Link>
+                <Link to="" className="btn btn-light">FaceBook</Link>
+            </div>
+        </div>
+        <div className="mycard">
+            <div className="cimg">
+                <img src={IMG13} alt="IMG6"/>
+            </div>
+            <div className="cdetail">
+                <h2>Sushoma Das.</h2>
+                <br/>
+                <br/>
+                <Link to="" className="btn">WebSite</Link>
+                <Link to="" className="btn btn-light">FaceBook</Link>
+            </div>
+        </div>
+        <div className="mycard">
+            <div className="cimg">
+                <img src={IMG14} alt="IMG6"/>
+            </div>
+            <div className="cdetail">
+                <h2>Sumon Mohanta.</h2>
+                <br/>
+                <br/>
+                <Link to="" className="btn">WebSite</Link>
+                <Link to="" className="btn btn-light">FaceBook</Link>
+            </div>
+        </div>
+        <div className="mycard">
+            <div className="cimg">
+                <img src={IMG15} alt="IMG6"/>
+            </div>
+            <div className="cdetail">
+                <h2>Rina Mohanta.</h2>
+                <br/>
+                <br/>
+                <Link to="" className="btn">WebSite</Link>
+                <Link to="" className="btn btn-light">FaceBook</Link>
+            </div>
+        </div>
+        <div className="mycard">
+            <div className="cimg">
+                <img src={IMG16} alt="IMG6"/>
+            </div>
+            <div className="cdetail">
+                <h2>Milon Mohanta.</h2>
+                <br/>
+                <br/>
+                <Link to="" className="btn">WebSite</Link>
+                <Link to="" className="btn btn-light">FaceBook</Link>
+            </div>
+        </div>
+        <div className="mycard">
+            <div className="cimg">
+                <img src={IMG17} alt="IMG6"/>
+            </div>
+            <div className="cdetail">
+                <h2>Bethe Mohanta.</h2>
+                <br/>
+                <br/>
+                <Link to="" className="btn">WebSite</Link>
+                <Link to="" className="btn btn-light">FaceBook</Link>
+            </div>
+        </div>
+        <div className="mycard">
+            <div className="cimg">
+                <img src={IMG18} alt="IMG6"/>
+            </div>
+            <div className="cdetail">
+                <h2>Kobita Mohanta.</h2>
+                <br/>
+                <br/>
+                <Link to="" className="btn">WebSite</Link>
+                <Link to="" className="btn btn-light">FaceBook</Link>
+            </div>
+        </div>
+        <div className="mycard">
+            <div className="cimg">
+                <img src={IMG19} alt="IMG6"/>
+            </div>
+            <div className="cdetail">
+                <h2>Rithu Mohanta.</h2>
+                <br/>
+                <br/>
+                <Link to="" className="btn">WebSite</Link>
+                <Link to="" className="btn btn-light">FaceBook</Link>
+            </div>
+        </div>
+        <div className="mycard">
+            <div className="cimg">
+                <img src={IMG20} alt="IMG6"/>
+            </div>
+            <div className="cdetail">
+                <h2>Subrata Dev Mohanta.</h2>
+                <br/>
+                <br/>
+                <Link to="" className="btn">WebSite</Link>
+                <Link to="" className="btn btn-light">FaceBook</Link>
+            </div>
+        </div>
+        <div className="mycard">
+            <div className="cimg">
+                <img src={IMG21} alt="IMG6"/>
+            </div>
+            <div className="cdetail">
+                <h2>Taposh Mohanta.</h2>
+                <br/>
+                <br/>
+                <Link to="" className="btn">WebSite</Link>
+                <Link to="" className="btn btn-light">FaceBook</Link>
+            </div>
+        </div>
+        <div className="mycard">
+            <div className="cimg">
+                <img src={IMG22} alt="IMG6"/>
+            </div>
+            <div className="cdetail">
+                <h2>Susmita Mohanta.</h2>
+                <br/>
+                <br/>
+                <Link to="" className="btn">WebSite</Link>
+                <Link to="" className="btn btn-light">FaceBook</Link>
+            </div>
+        </div>
+        <div className="mycard">
+            <div className="cimg">
+                <img src={IMG23} alt="IMG6"/>
+            </div>
+            <div className="cdetail">
+                <h2>Udvash Mohanta.</h2>
+                <br/>
+                <br/>
+                <Link to="" className="btn">WebSite</Link>
+                <Link to="" className="btn btn-light">FaceBook</Link>
+            </div>
+        </div>
+        <div className="mycard">
+            <div className="cimg">
+                <img src={IMG24} alt="IMG6"/>
+            </div>
+            <div className="cdetail">
+                <h2>Sreeja Mohanta.</h2>
+                <br/>
+                <br/>
+                <Link to="" className="btn">WebSite</Link>
+                <Link to="" className="btn btn-light">FaceBook</Link>
+            </div>
+        </div>
+        <div className="mycard">
+            <div className="cimg">
+                <img src={IMG25} alt="IMG6"/>
+            </div>
+            <div className="cdetail">
+                <h2>Aaradhya Mohanta.</h2>
+                <br/>
+                <br/>
+                <Link to="" className="btn">WebSite</Link>
+                <Link to="" className="btn btn-light">FaceBook</Link>
+            </div>
+        </div>
+        <div className="mycard">
+            <div className="cimg">
+                <img src={IMG26} alt="IMG6"/>
+            </div>
+            <div className="cdetail">
+                <h2>Sarollo Mohanta.</h2>
                 <br/>
                 <br/>
                 <Link to="" className="btn">WebSite</Link>
